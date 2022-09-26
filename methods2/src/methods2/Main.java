e52a022293f8efd74826b345b1acf3107186d145
güvenli enerji <gvenli enerji@192.168.1.33>
2022-09-26 15:24:05 +0300

    variable arguments

--- methods2/src/methods2/Main.java
+++ methods2/src/methods2/Main.java
@@ -7,7 +7,8 @@ public class Main {
 		String yeniMesaj=sehirVer();
 		System.out.println(yeniMesaj);
 		int sayi =topla(5,7);
-		System.out.println(sayi);
+		int toplam=topla2(1,2,3,4,5,6,7);
+		System.out.println(toplam);
 					}
 	public static void ekle() {
 		System.out.println("Eklendi");
@@ -21,6 +22,14 @@ public class Main {
 	public static int topla(int sayi1,int sayi2) {
 		return sayi1+sayi2;
 	}
+	public static int topla2(int... sayilar) {
+		int toplam=0;
+		for(int sayi:sayilar) {
+			toplam+=sayi;
+		}
+		return toplam;
+	}
+	
 	public static String sehirVer() {
 		return "Ankara";
 	}
