2eb5cfeb0c9f6f339260cab3d085f11d0128b677
güvenli enerji <gvenli enerji@192.168.1.33>
2022-09-26 16:18:01 +0300

    Sesli Harfler switch case

---
+++ SesliHarfler/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char harf='A';
+		
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U': {
+			
+			System.out.println("Kalıın sesli harf");
+			break;
+		}
+		default:
+			System.out.println("İnce sesli harf");
+			}
+
+	}
+
+}
